2d592de06b84b0d88a9f9ef02885a61763917588
Lyne <lyne.root@gmail.com>
2018-02-06 16:06:24 +0800

    test

--- flaskproj.py
+++ flaskproj.py
@@ -1,12 +1,18 @@
 from flask import Flask, render_template
 from flask_script import Manager
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
-
+db = SQLAlchemy(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
