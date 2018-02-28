e74fbc2ac26102b1dfbdf96af6f6e0d296ee2b41
Lyne <lyne.root@gmail.com>
2018-02-28 18:01:05 +0800

    change password

--- app/models.py
+++ app/models.py
@@ -53,8 +53,8 @@ class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(64), unique=True, index=True)
     username = db.Column(db.String(64), unique=True, index=True)
-    password_hash = db.Column(db.String(128))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    password_hash = db.Column(db.String(128))
     confirmed = db.Column(db.Boolean, default=False)
     name = db.Column(db.String(64))
     location = db.Column(db.String(64))
@@ -70,9 +70,6 @@ class User(UserMixin, db.Model):
             if self.role is None:
                 self.role = Role.query.filter_by(default=True).first()
 
-    def __repr__(self):
-        return '<User %r>' % self.username
-
     @property
     def password(self):
         raise AttributeError('password is not a readable attribute!')
@@ -83,10 +80,6 @@ class User(UserMixin, db.Model):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
     def generate_confirmation_token(self, expiration=3600):
         s = TimedJSONWebSignatureSerializer(current_app.config['SECRET_KEY'], expiration)
@@ -114,6 +107,8 @@ class User(UserMixin, db.Model):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
 
+    def __repr__(self):
+        return '<User %r>' % self.username
 
 class AnonymousUser(AnonymousUserMixin):
     def can(self, permissions):
@@ -124,3 +119,8 @@ class AnonymousUser(AnonymousUserMixin):
 
 
 login_manager.anonymous_user = AnonymousUser
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
